ce09b531c9c2e234022306de371357a53a828df8
Rahulkumar Choudhary <59869006+Rahulkumar0909@users.noreply.github.com>
2021-06-03 18:52:35 +0530

    Update Security_Alert_Mail_by_Cam.py

--- Security_Alert_Mail_by_Cam.py
+++ Security_Alert_Mail_by_Cam.py
@@ -1,62 +1,5 @@
 import cv2
-
-from datetime import date
-from datetime import datetime
-
-import smtplib
-from email.mime.text import MIMEText
-from email.mime.multipart import MIMEMultipart
-from email.mime.base import MIMEBase
-from email import encoders
-
-def Security_mail():
-    cv2.imwrite("image.jpg", cphoto)
-
-    email_user='user@mail.com'
-    email_send='reciver@mail.com'
-    email_user_pass='PASSWORDXXXXXXXXXXXXXX'
-
-    subject= '⚠ Security Alert!! ⚠'
-
-    current_time=datetime.datetime.now()
-
-    msg=MIMEMultipart()
-    msg['From']=email_user
-    msg['To']=email_send
-    msg['Subject']=subject
-
-    today = date.today()
-
-    dtime= now.strftime("%H:%M:%S")
-    ddate=today.strftime("%B %d, %Y")
-
-    body = '''
-    ⚠ Security Alert! ⚠
-    Someone has been spotted in the camara.
-
-    During:-
-    Date: '''+ddate+'''
-    Time: '''+dtime
-
-    msg.attach(MIMEText(body,'plain'))
-
-    filename='image.jpg'
-    attachment =open(filename, 'rb')
-
-    part = MIMEBase('application', 'octet-stream')
-    part.set_payload((attachment).read())
-    encoders.encode_base64(part)
-    part.add_header('Content-Disposition', "attachment; filename="+filename)
-
-    msg.attach(part)
-    text= msg.as_string()
-    server = smtplib.SMTP('smtp.gmail.com',587)
-    server.starttls()
-    server.login(email_user,email_user_pass)
-
-    server.sendmail(email_user,email_send,text)
-    server.quit()
-    
+import mail
     
 cap=cv2.VideoCapture(0)
 model=cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
@@ -81,7 +24,7 @@ while True:
         if cv2.waitKey(10)==13:
             break
 
-Security_mail()
+mail.securityMail()
 
 cv2.destroyAllWindows()
 cap.release()
