c2ef74ce90c7b292ecc7dc8719fdeb43a8c33e2f
Om Kathe <omkathe26@gmail.com>
2021-07-08 12:26:59 +0530

    comments

--- mail.py
+++ mail.py
@@ -2,9 +2,13 @@
 #Enter reciever mail ID in variable email_send
 #Enter password of the user mail ID in email_user_pass
 
+#smtplib to send email
 import smtplib
+#MIMEText for plain text
 from email.mime.text import MIMEText
+#MIMEMultipart for multipart form request
 from email.mime.multipart import MIMEMultipart
+#Base class MIMEBase
 from email.mime.base import MIMEBase
 from email import encoders
 
@@ -20,15 +24,16 @@ def securityMail():
     email_user_pass='PASSWORDXXXXXXXXXXXXXX'
 
     subject= '⚠ Security Alert!! ⚠'
-    
+    #Multipart message as we have image and text both
     msg=MIMEMultipart()
+    #Headers
     msg['From']=email_user
     msg['To']=email_send
     msg['Subject']=subject
 
     dtime= now.strftime("%H:%M:%S")
     ddate=today.strftime("%B %d, %Y")
-
+    #Body of email
     body = '''
 ⚠ Security Alert! ⚠
 Someone has been spotted in the camara.
@@ -40,18 +45,23 @@ Time: '''+dtime+'''
 Front face of the person in jpg format:
 
 '''
-
+    #Adding text to multipart 
     msg.attach(MIMEText(body,'plain'))
-
+    #Open the image
     filename='image.jpg'
     attachment =open(filename, 'rb')
 
     part = MIMEBase('application', 'octet-stream')
+    #Attaching the content
     part.set_payload((attachment).read())
+    #Enclode image in base64 string
     encoders.encode_base64(part)
+    #Content-Disposition to indicate if content will be visisble or downloadable. In this case downloadable, as attachment option is selected
     part.add_header('Content-Disposition', "attachment; filename="+filename)
-
+    
+    #Attaching the image to message
     msg.attach(part)
+    #Covert into string
     text= msg.as_string()
     server = smtplib.SMTP('smtp.gmail.com',587)
     server.starttls()
